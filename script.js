fd7c34b86f7ec851892992c26c54e83f26b1c86a
kwright73 <kayceewright@gmail.com>
2024-08-21 01:03:13 -0400

    commit java refresher

---
+++ script.js
@@ -0,0 +1,27 @@
+let firstName = "Kaycee";
+let lastName = "Wright";
+let fullName = firstName + lastName;
+console.log(fullName);
+
+let population = Number(897720);
+console.log(population);
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+
+let red = "red";
+let orange = "orange";
+let yellow = "yellow";
+let green = "green";
+let purple = "purple";
+let blue = "blue";
+
+let colors = [red, orange, yellow, green, purple, blue];
+
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
